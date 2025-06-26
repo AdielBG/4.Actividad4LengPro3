452f4823db19c08099349899f035a09e2d0360eb
AdielBG <adielbg17.2@gmail.com>
2025-06-26 11:48:10 -0400

    Copia del proyecto anterior

---
+++ Actividad4LengProg3/Actividad4LengProg3/Controllers/EstudiantesController.cs
@@ -0,0 +1,112 @@
+﻿using Actividad4LengProg3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace Actividad4LengProg3.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        public static List<EstudianteViewModel> estudiantes = new List<EstudianteViewModel>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Registrar(EstudianteViewModel estudiante)
+        {
+            if (ModelState.IsValid) //Verificar si los datos del modelo que se envían son válidos.
+            {
+                estudiantes.Add(estudiante);
+                TempData["Mensaje"] = "Estudiante registrado exitosamente.";
+                return RedirectToAction("Lista"); //Redirigir al usuario a la accion Lista
+
+            }
+
+            return View();
+        }
+
+
+        public IActionResult Lista()
+        {
+
+            return View(estudiantes);
+
+        }
+
+        [HttpGet]
+        public IActionResult Editar(string matricula)
+        {
+            var estudiante = estudiantes.FirstOrDefault(e => e.Matricula == matricula);
+            if (estudiante == null)
+            {
+                TempData["Mensaje"] = "No existe el usuario indicado";
+                return RedirectToAction("Lista");
+            }
+
+            return View(estudiante);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Editar(EstudianteViewModel estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                EstudianteViewModel original = estudiantes.FirstOrDefault(e => e.Matricula.Equals(estudiante.Matricula));
+
+                if (original == null)
+                {
+                    TempData["Mensaje"] = "No existe el usuario indicado";
+                    return RedirectToAction("Lista");
+                }
+
+                // Actualiza los campos necesarios
+                original.NombreCompleto = estudiante.NombreCompleto;
+                original.Carrera = estudiante.Carrera;
+                original.CorreoInstitucional = estudiante.CorreoInstitucional;
+                original.Telefono = estudiante.Telefono;
+                original.FechaNacimiento = estudiante.FechaNacimiento;
+                original.Genero = estudiante.Genero;
+                original.Turno = estudiante.Turno;
+                original.TipoIngreso = estudiante.TipoIngreso;
+                original.PorcentajeBeca = estudiante.PorcentajeBeca;
+                //original.TerminosYCondiciones = estudiante.TerminosYCondiciones;
+
+                TempData["Mensaje"] = "Actualizaciones realizadas correctamente";
+                return RedirectToAction("Lista");
+            }
+
+            return View(estudiante);
+        }
+
+
+
+        public IActionResult Eliminar(string matricula)
+        {
+            var estudiante = estudiantes.FirstOrDefault(e => e.Matricula == matricula);
+            if (estudiante == null)
+            {
+                TempData["Mensaje"] = "No existe el usuario indicado";
+                return RedirectToAction("Lista");
+            }
+            return View(estudiante);
+        }
+
+        [HttpPost]
+        public IActionResult EliminarConfirmado(string matricula)
+        {
+            var estudiante = estudiantes.FirstOrDefault(e => e.Matricula == matricula);
+            if (estudiante != null)
+            {
+                TempData["Mensaje"] = "Estudiante sustraido correctamente";
+                estudiantes.Remove(estudiante);
+            }
+
+            return RedirectToAction("Lista");
+        }
+
+    }
+}
---
+++ Actividad4LengProg3/Actividad4LengProg3/Models/EstudianteViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Actividad4LengProg3.Models
+{
+    public class EstudianteViewModel
+    {
+        [Required(ErrorMessage = "Debe ingresar su Nombre")]
+        [StringLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres.")]
+        public string NombreCompleto { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar su Matricula")]
+        [StringLength(15, MinimumLength = 6, ErrorMessage = "La matrícula debe tener entre 6 y 15 caracteres.")]
+        public string Matricula { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar una carrera")]
+        public string Carrera { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar su Correo Institucional")]
+        [EmailAddress(ErrorMessage = "Debe ser un correo válido.")]
+        public string CorreoInstitucional { get; set; }
+
+        [Phone(ErrorMessage = "Debe ingresar un número de teléfono válido.")]
+        [MinLength(10, ErrorMessage = "El teléfono debe tener al menos 10 dígitos.")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar su Fecha de Nacimiento")]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+
+        [Required(ErrorMessage = "Debe seleccionar un género.")]
+        public string Genero { get; set; }
+
+
+        [Required(ErrorMessage = "Debe seleccionar un turno.")]
+        public string Turno { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar el tipo de ingreso.")]
+        public string TipoIngreso { get; set; }
+
+
+        public bool EstaBecado { get; set; }
+
+
+        [Range(0, 100, ErrorMessage = "El porcentaje debe estar entre 0 y 100.")]
+        public int PorcentajeBeca { get; set; }
+
+        //[Required(ErrorMessage = "Debe aceptar los términos y condiciones.")]
+        //[Range(typeof(bool), "true", "true")]
+        //public string TerminosYCondiciones { get; set; }
+
+        [Required(ErrorMessage = "Debe aceptar los términos y condiciones.")]
+        public bool TerminosYCondiciones { get; set; }
+    }
+}
