065ad55865dbb8bb11d6e3b1efe2f17eb1bea81a
AdielBG <adielbg17.2@gmail.com>
2025-06-29 11:50:09 -0400

    MateriasController - Index Materias

---
+++ Actividad4LengProg3/Actividad4LengProg3/Controllers/MateriasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Actividad4LengProg3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad4LengProg3.Controllers
+{
+    public class MateriasController : Controller
+    {
+        private readonly AppDbContext _context;
+        public MateriasController(AppDbContext context) { 
+        
+            _context = context;
+        
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
