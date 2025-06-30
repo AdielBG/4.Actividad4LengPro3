db1f0c238726f1a868b4692bda3dced4fe5c920b
AdielBG <adielbg17.2@gmail.com>
2025-06-30 12:15:04 -0400

    Acción Registrar Materias - Acción Lista Materias

--- Actividad4LengProg3/Actividad4LengProg3/Controllers/MateriasController.cs
+++ Actividad4LengProg3/Actividad4LengProg3/Controllers/MateriasController.cs
@@ -16,5 +16,26 @@ namespace Actividad4LengProg3.Controllers
         {
             return View();
         }
+
+
+        [HttpPost]
+        public IActionResult Registrar(MateriaViewModel materia)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Materias.Add(materia);
+                _context.SaveChanges();
+                TempData["Mensaje"] = "Materia registrada exitosamente.";
+                return RedirectToAction("Lista");
+            }
+            return View(materia);
+        }
+
+        public IActionResult Lista()
+        {
+            var materias = _context.Materias.ToList();
+            return View(materias);
+        }
+
     }
 }
