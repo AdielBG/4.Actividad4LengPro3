253f47a3e278dc2407d47f1ac9611c1dba4b4024
AdielBG <adielbg17.2@gmail.com>
2025-07-01 11:52:48 -0400

    Editar Materias

--- Actividad4LengProg3/Actividad4LengProg3/Controllers/MateriasController.cs
+++ Actividad4LengProg3/Actividad4LengProg3/Controllers/MateriasController.cs
@@ -7,10 +7,11 @@ namespace Actividad4LengProg3.Controllers
     public class MateriasController : Controller
     {
         private readonly AppDbContext _context;
-        public MateriasController(AppDbContext context) { 
-        
+        public MateriasController(AppDbContext context)
+        {
+
             _context = context;
-        
+
         }
         public IActionResult Index()
         {
@@ -37,5 +38,78 @@ namespace Actividad4LengProg3.Controllers
             return View(materias);
         }
 
+        [HttpGet]
+        public IActionResult Editar(string codigo)
+        {
+            var materia = _context.Materias.FirstOrDefault(m => m.Codigo == codigo);
+            if (materia == null)
+            {
+                TempData["Mensaje"] = "No existe la materia indicado";
+                return RedirectToAction("Lista");
+            }
+
+            return View(materia);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Editar(MateriaViewModel materia)
+        {
+            if (ModelState.IsValid)
+            {
+                var original = _context.Materias.FirstOrDefault(m => m.Codigo == materia.Codigo);
+
+                if (original == null)
+                {
+                    TempData["Mensaje"] = "No existe la materia indicado";
+                    return RedirectToAction("Lista");
+                }
+
+                // Actualizar campos
+                original.Codigo = materia.Codigo;
+                original.Nombre = materia.Nombre;
+                original.Creditos = materia.Creditos;
+                original.Carrera = materia.Carrera;
+
+                _context.Update(original);
+                _context.SaveChanges();
+
+                TempData["Mensaje"] = "Actualizaciones realizadas correctamente";
+                return RedirectToAction("Lista");
+            }
+
+            return View(materia);
+        }
+
+
+
+        //    public IActionResult Eliminar(string matricula)
+        //    {
+        //        var estudiante = _context.Estudiantes.FirstOrDefault(e => e.Matricula == matricula);
+        //        if (estudiante == null)
+        //        {
+        //            TempData["Mensaje"] = "No existe el usuario indicado";
+        //            return RedirectToAction("Lista");
+        //        }
+        //        return View(estudiante);
+        //    }
+
+        //    [HttpPost]
+        //    public IActionResult EliminarConfirmado(string matricula)
+        //    {
+        //        var estudiante = _context.Estudiantes.FirstOrDefault(e => e.Matricula == matricula);
+        //        if (estudiante != null)
+        //        {
+        //            _context.Estudiantes.Remove(estudiante);
+        //            _context.SaveChanges(); //  Necesario
+        //            TempData["Mensaje"] = "Estudiante eliminado correctamente";
+        //        }
+
+        //        return RedirectToAction("Lista");
+        //    }
+
+
+        //}
     }
 }
