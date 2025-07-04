ede1127af83d0df195e0471ee41fdec00471a902
AdielBG <adielbg17.2@gmail.com>
2025-07-04 15:54:22 -0400

    Calificaciones Controller

---
+++ Actividad4LengProg3/Actividad4LengProg3/Controllers/CalificacionesController.cs
@@ -0,0 +1,21 @@
+﻿using Actividad4LengProg3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad4LengProg3.Controllers
+{
+    public class CalificacionesController : Controller
+    {
+        private readonly AppDbContext _contex;
+
+        public CalificacionesController(AppDbContext contex)
+        {
+            _contex = contex;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
